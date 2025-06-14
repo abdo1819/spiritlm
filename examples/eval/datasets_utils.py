a9f6684a5a0bc8579aed1c667ee871b0bfd1dee1
abdo ragab hashem <abdo14hashem15@outlook.com>
2025-06-14 19:14:42 +0300

    Log predictions and references

--- examples/eval/datasets_utils.py
+++ examples/eval/datasets_utils.py
@@ -8,7 +8,7 @@ import torchaudio
 from datasets import load_dataset  # type: ignore
 
 
-Example = Tuple[torch.Tensor, int, str]
+Example = Tuple[torch.Tensor, int, str, str]
 
 
 def _load_librispeech_from_hf(subset: str) -> Iterable[Example]:
@@ -29,12 +29,13 @@ def _load_librispeech_from_hf(subset: str) -> Iterable[Example]:
 
     hf_config, hf_split = _hf_config_and_split(subset)
     print(f"loading librispeech_asr , config {hf_config} split {hf_split}")
-    hf_dataset = load_dataset("librispeech_asr", hf_config, split=hf_split,streaming=True)
+    hf_dataset = load_dataset("librispeech_asr", hf_config, split=hf_split, streaming=True)
     for sample in hf_dataset:
         yield (
             torch.tensor(sample["audio"]["array"]),
             sample["audio"]["sampling_rate"],
             sample.get("text", ""),
+            str(sample.get("id", sample.get("file", ""))),
         )
 
 
@@ -50,6 +51,7 @@ def _load_gigaspeech_from_hf(subset: str) -> Iterable[Example]:
             torch.tensor(sample["audio"]["array"]),
             sample["audio"]["sampling_rate"],
             sample["text"],
+            str(sample.get("segment_id", sample.get("id", ""))),
         )
 
 
